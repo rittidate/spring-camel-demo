f4daaf9a129e040a765d3d389d21e62b3ab21431
Rittidate Kittisarathamma <rittidate.k@gmail.com>
2024-12-24 01:17:58 +0700

    condition and operator

---
+++ camel-microservice-a/src/main/java/com/rittidate/microservices/camel_microservice_a/routes/a/MyFileRouter.java
@@ -0,0 +1,25 @@
+package com.rittidate.microservices.camel_microservice_a.routes.a;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyFileRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("file:files/input")
+        .routeId("Files-Input-Route")
+        .transform().body(String.class)
+        .choice()
+            .when(simple("${file:ext} ends with 'xml'"))
+                .log("XML FILE")
+            .when(simple("${body} contains 'USD'"))
+                .log("Not XML FILE BUT contains USD")
+            .otherwise()
+                .log("Not an XML FILE")
+        .end()
+        .log("${messageHistory} ${headers.CamelFileAbsolute}")
+        .to("file:files/output");
+    }
+}
