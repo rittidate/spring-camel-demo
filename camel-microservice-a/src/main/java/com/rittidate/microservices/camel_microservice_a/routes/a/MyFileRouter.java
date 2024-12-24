845a5271864598b3d7a411be77df39af7f047592
Rittidate Kittisarathamma <rittidate.k@gmail.com>
2024-12-24 11:38:45 +0700

    change to bean in choice

--- camel-microservice-a/src/main/java/com/rittidate/microservices/camel_microservice_a/routes/a/MyFileRouter.java
+++ camel-microservice-a/src/main/java/com/rittidate/microservices/camel_microservice_a/routes/a/MyFileRouter.java
@@ -1,10 +1,21 @@
 package com.rittidate.microservices.camel_microservice_a.routes.a;
 
+import org.apache.camel.ExchangeProperties;
+import org.apache.camel.Headers;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
 
 @Component
 public class MyFileRouter extends RouteBuilder {
+
+    @Autowired
+    private DeciderBean deciderBean;
 
     @Override
     public void configure() throws Exception {
@@ -14,7 +25,8 @@ public class MyFileRouter extends RouteBuilder {
         .choice()
             .when(simple("${file:ext} ends with 'xml'"))
                 .log("XML FILE")
-            .when(simple("${body} contains 'USD'"))
+            .when(method(deciderBean))
+//            .when(simple("${body} contains 'USD'"))
                 .log("Not XML FILE BUT contains USD")
             .otherwise()
                 .log("Not an XML FILE")
@@ -28,5 +40,17 @@ public class MyFileRouter extends RouteBuilder {
         .log("${file:onlyname.noext} ${file:parent} ${file:path} ${file:absolute}")
         .log("${file:size} ${file:modified}")
         .log("${routeId} ${camelId} ${body}");
+    }
+}
+
+@Component
+class DeciderBean {
+    Logger logger = LoggerFactory.getLogger(DeciderBean.class);
+
+    public boolean isThisConditionMet(String body,
+                                      @Headers Map<String, String> headers,
+                                      @ExchangeProperties Map<String, String> exchangeProperties) {
+        logger.info("DeciderBean {} {} {}", body, headers, exchangeProperties);
+        return true;
     }
 }
